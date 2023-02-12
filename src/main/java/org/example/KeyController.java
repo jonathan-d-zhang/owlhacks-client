10b16ae62647eaaa4e0a1403c448cba7e41ab45e
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2023-02-11 21:40:32 -0500

    Receive input from TextView

--- src/main/java/org/example/KeyController.java
+++ src/main/java/org/example/KeyController.java
@@ -3,18 +3,22 @@ package org.example;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.http.HttpClient;
 
 public class KeyController {
-    private HttpClient client;
+    private HttpClient client = HttpClient.newBuilder().build();
+
+    @FXML TextField textField;
 
     @FXML
-    TextField textField;
-    private void submitKey() {
-        if (client == null) {
-            client = HttpClient.newBuilder().build();
-        }
+    private void submitKey() throws IOException {
+        System.out.println("called submitKey");
 
-        System.out.println(textField.getText());
+        try {
+            int key = Integer.parseInt(textField.getText());
+            System.out.println("Inputted: " + key);
+            App.setRoot("textview");
+        } catch (NumberFormatException ignored) {}
     }
 }
